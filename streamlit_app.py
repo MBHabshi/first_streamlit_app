a3e2ad4e8dabe33acfdb60222e492fad68fe9f15
MBHabshi <93523970+MBHabshi@users.noreply.github.com>
2023-02-21 11:05:05 +0000

    Update streamlit_app.py
    Added Dataframe for fruitlist

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+
 
 streamlit.title('My Parents new healthy diner')
 
@@ -7,4 +13,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 1-Porridge')
 streamlit.text('🍞2-French toast')
 streamlit.text('3-Coffee')
-
+streamlit.dataframe(my_fruit_list)

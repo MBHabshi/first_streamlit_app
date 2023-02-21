3d7815413185585100c640c4598497b759730b63
MBHabshi <93523970+MBHabshi@users.noreply.github.com>
2023-02-21 10:54:10 +0000

    Update streamlit_app.py
    Added menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 
 streamlit.title('My Parents new healthy diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('1-Porridge')
+streamlit.text('2-French toast')
+streamlit.text('3-Coffee')
+

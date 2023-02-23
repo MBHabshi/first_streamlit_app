a4ac0f296b9507617cf43fb10fdb813510f35609
MBHabshi <93523970+MBHabshi@users.noreply.github.com>
2023-02-23 12:07:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def get_fruit_load_list():
 
 # Add buttton to load the fruit
 if streamlit.button('Get Fruit Load List'):    
-    my_data_row = get_fruit_load_list()
+    my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
 
